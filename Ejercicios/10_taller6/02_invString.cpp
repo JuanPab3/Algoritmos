028d0a942c463cf944c79dd605ce125312b718a6
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-03-03 09:39:04 -0500

    Adelanto taller 6
    issue no: AD026

--- Ejercicios/10_taller6/02_invString.cpp
+++ Ejercicios/10_taller6/02_invString.cpp
@@ -7,16 +7,16 @@ string invString(string str, int first, int last);
 
 int main() {
   string a = "123456789",b;
+  cout << "Normal:  " << a << endl;
   int size = a.size()-1;
   b = invString(a,0,size);
-  cout << b << endl;
+  cout << "Inverse: " << b << endl;
 
   return 0;
 }
 
 
 string invString(string str, int first, int last){
-  int count = 0;
   if (last < first){
     return str;
   } else {
---
+++ Ejercicios/10_taller6/03_isPalindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string word);
+
+int main() {
+
+  cout << isPalindrome("oso") << endl;
+
+  return 0;
+}
+
+bool isPalindrome(string word){
+
+
+
+}
