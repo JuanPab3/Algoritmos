b1b64bc9b45d09898a646abb74a8e45d7a21443e
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-03-28 11:02:51 -0500

    Taleer 6 - Ej:06 terminado
    issue no: UD032

--- Ejercicios/10_taller6/06_reverseVector.cpp
+++ Ejercicios/10_taller6/06_reverseVector.cpp
@@ -3,23 +3,42 @@
 
 using namespace std;
 
-void  reverse_vector(vector < auto > & vec);
+void  reverse_vector(vector < int > & vec);
 
 int main() {
-  vector < int > vec;
+    vector < int > vec;
+    for (int i = 0; i <= 10; ++i)
+        vec.push_back(i);
 
+    cout << "Normal:" << endl << "{ ";
+    for (int elem : vec)
+        cout << elem << " ";
+    cout << "}" << endl;
+
+    reverse_vector(vec);
+
+    cout << "Reverse:" << endl << "{ ";
+    for (int elem : vec)
+        cout << elem << " ";
+    cout << "}" << endl;
 
   return 0;
 }
 
 void  reverse_vector(vector < int > & vec){
-  if (vec.size() <= 1){
-    vec = vec;
-  } else {
+    if (vec.size() <= 1){
+        vec = vec;
+    } else {
+        vector<int>::iterator it = vec.begin();
+        int first = *it ,last = *(vec.end()-1);
 
+        vec.erase(it);
+        vec.pop_back();
 
+        reverse_vector(vec);
 
-
+        vec.insert(it,last);
+        vec.push_back(first);
 
   }
 }
