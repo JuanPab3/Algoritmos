c187b28f38d6f6b97e1d1129a10b99c25be4a1ea
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2020-03-29 07:31:59 -0500

    Taller 6 terminado
    issue no: UD036

--- Ejercicios/10_taller6/05_stringPermutations.cpp
+++ Ejercicios/10_taller6/05_stringPermutations.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
+#include <string>
 #include <set>
 
 using namespace std;
 
-set  permutaciones(string & s);
+set<string> permutaciones(string & s);
+set<string> char_changer(set<string> & s, char c);
 
 int main() {
 
-  return 0;
+    string word;
+    cout << "Word 4 permutations; ";
+    cin >> word;
+
+    set<string> perm = permutaciones(word);
+
+    for (set<string>::iterator it = perm.begin(); it != perm.end(); ++it)
+        cout << *it << endl;
+
+
+    return 0;
 }
 
-set  permutaciones(string & s){
-  
+set<string> permutaciones(string & s){
+    set<string> perm;
+    if (s.size() <= 1) {
+        perm.insert(s);
+    } else {
+        char first = s.at(0);
+        string tmp = s.substr(1,s.size()-1);
+        set<string> set_s = permutaciones(tmp);
+        perm = char_changer(set_s,first);
+    }
+    return perm;
+}
+
+set<string> char_changer(set<string> & s, char c){
+    set<string> out;
+    string str, temp, ch(1,c);
+    for (set<string>::iterator it = s.begin(); it != s.end(); ++it)  {
+        str = *it;
+        for (size_t i = 0; i <= str.size(); ++i) {
+            temp = str;
+            temp.insert(i,ch);
+            out.insert(temp);
+        }
+    }
+    return out;
 }
